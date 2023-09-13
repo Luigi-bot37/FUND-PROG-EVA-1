d00f721bdea3e08f51e4d49b32c77957ee5b7a2a
Luigi-bot37 <143562859+Luigi-bot37@users.noreply.github.com>
2023-09-13 17:56:31 -0600

    Add files via upload

---
+++ EVA1_9_IDENTIFICADORES/src/eva1_9_identificadores/EVA1_9_IDENTIFICADORES.java
@@ -0,0 +1,22 @@
+package eva1_9_identificadores;
+
+public class EVA1_9_IDENTIFICADORES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // int 1nombre; no es valido, inicia con numero
+        int $valor;
+        int _valor;
+        int valor;
+        int valor1;
+        int VALOR;
+        int Valor;
+        //int VALOR; no es valido por que ya existe
+        //int double; no es valido por que imita una palabra reservada
+        int Double; //no da error porquelo lee como un identificador normal
+        double fdsmdbr; //es valido, pero no se entiende cual es su funcion
+    }
+
+}
