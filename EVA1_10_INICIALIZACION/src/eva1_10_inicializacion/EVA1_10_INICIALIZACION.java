dcc5ac314bc21590fc8e9b10466594ef7b253e06
Luigi-bot37 <143562859+Luigi-bot37@users.noreply.github.com>
2023-09-18 17:53:34 -0600

    Add files via upload

---
+++ EVA1_10_INICIALIZACION/src/eva1_10_inicializacion/EVA1_10_INICIALIZACION.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva1_10_inicializacion;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA1_10_INICIALIZACION {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int valor = 0;//Inicializando la variable
+        System.out.println(valor);
+        int precio; //declarar
+        precio = 100; //inicializar
+        double salario = 0, promedio = 10, calif = 9.5, iva = 16, total = 123.23;
+        //5 variables de tipo double, separadas por coma
+        //CONSTANTES
+        //final---> SOLO asigna un valor una sola vez
+        final int CONTEO;
+        CONTEO = 100;
+        System.out.println(CONTEO);
+        final double PI = 3.1416;
+        final String ESCUELA = "INSTITUTO TECNOLIGICO DE CHIHUAHUA II";
+        final int CALIF_PASAR = 70;
+
+        /* BLOQUE DE COMENTARIOS
+       UTIL PARA MUCHO TEXTO
+         */
+        //COMENTARIO DE UNA SOLA LINEA, SOLO DENTRO DE ESTA LINEA ES COMENTARIO
+    }
+
+}
